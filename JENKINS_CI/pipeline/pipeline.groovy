637f2a557eaabc232d877e7018c1eb7fa89e1c59
alkcode16 <alek_1604@hotmail.com>
2025-09-21 23:51:48 -0600

    Configuracion DnD

--- JENKINS_CI/pipeline/pipeline.groovy
+++ JENKINS_CI/pipeline/pipeline.groovy
@@ -5,6 +5,9 @@ pipeline {
         DB_URL = 'jdbc:postgresql://db:5432/gestion_usuarios_db'
         DB_USER = 'disenio'
         DB_PASS = 'd153N10'
+        
+        // Define el nombre de la imagen Docker que vas a construir
+        DOCKER_IMAGE = "user-role-based-app:${env.BUILD_ID}"
     }
 
     options {
@@ -13,24 +16,16 @@ pipeline {
     }
 
     triggers {
-        pollSCM('H/2 * * * *') // Revisa cambios cada 2 minutos
+        pollSCM('H/2 * * * *')
     }
 
     stages {
-        
-        stage('Kill Previous Instance') {
-            steps {
-                sh 'pkill -f "java -jar" || true'
-            }
-        }
-
         stage('Checkout') {
             steps {
-                // git branch: 'prueba', git credentialsId: 'github-jenkins', url: 'https://github.com/Jaim24M/UserRoleBasedTemplate.git'
                 git branch: 'prueba', credentialsId: 'github-jenkins', url: 'https://github.com/Jaim24M/UserRoleBasedTemplate.git'
             }
         }
-
+        
         stage('Build') {
             steps {
                 sh 'chmod +x mvnw'
@@ -45,16 +40,31 @@ pipeline {
             }
         }
         
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    sh "docker build -t ${DOCKER_IMAGE} ."
+                }
+            }
+        }
+        
         stage('Deploy') {
             steps {
-                sh "nohup java -jar target/*.jar --spring.datasource.url=${DB_URL} --spring.datasource.username=${DB_USER} --spring.datasource.password=${DB_PASS} &"
+                script {
+                    // Detiene y elimina el contenedor si ya existe
+                    sh "docker stop user-app || true"
+                    sh "docker rm user-app || true"
+
+                    // Inicia un nuevo contenedor con la imagen recién construida
+                    sh "docker run -d --name user-app -p 8080:8080 --env-file <(printf 'SPRING_DATASOURCE_URL=${DB_URL}\\nSPRING_DATASOURCE_USERNAME=${DB_USER}\\nSPRING_DATASOURCE_PASSWORD=${DB_PASS}') ${DOCKER_IMAGE}"
+                }
             }
         }
     }
 
     post {
         success {
-            echo '✅ Despliegue exitoso'
+            echo '✅ Despliegue exitoso en contenedor Docker'
         }
         failure {
             echo '❌ Falló el pipeline'
